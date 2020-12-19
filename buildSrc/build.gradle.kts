62763be4781bba051177ff08f5765e069c7ee549
Mao <lcdsmao@gmail.com>
2020-12-19 11:41:35 +0900

    Bump AGP to 7.0.0-alpha03

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,9 +13,6 @@ dependencies {
   implementation(GradlePlugin.bintray)
   implementation(GradlePlugin.artifactory)
   implementation(GradlePlugin.detekt)
-  // FIXME: Remove these two transitive dependencies
-  implementation("com.android.tools.build:builder:_")
-  implementation("com.android.tools.build:builder-model:_")
 }
 
 object GradlePlugin {
